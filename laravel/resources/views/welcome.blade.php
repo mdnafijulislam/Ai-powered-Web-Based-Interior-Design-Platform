96b96da6658f7b0d8fd980502b640162ee5bcfd9
mdnafijulislam <islam22205101045@diu.edu.bd>
2025-11-26 01:08:53 +0600

    test change

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -6,8 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- Repo: https://github.com/mdnafijulislam/Ai-powered-Web-Based-Interior-Design-Platform.git -->
-    <!-- To push this project on Windows run: c:\php project\push_to_github.bat -->
     <h1>Welcome to Laravel</h1>
+    <p>This is the welcome page.</p>
 </body>
 </html>
