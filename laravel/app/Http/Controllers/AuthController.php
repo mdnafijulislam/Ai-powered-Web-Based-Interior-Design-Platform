ff3a43b48bbcce39f9380e6401d7835ca953e67b
mdnafijulislam <islam22205101045@diu.edu.bd>
2025-11-27 16:29:36 +0600

    authcontroller update

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -16,3 +16,33 @@ class AuthController extends Controller
         return view('auth.register');
     }
 }
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // Show Register Page
+    public function showRegister() {
+        return view('auth.register');
+    }
+
+    // Register form submit
+    public function register(Request $request) {
+
+        // Basic validation placeholder
+        $request->validate([
+            'name'     => 'required',
+            'email'    => 'required|email',
+            'password' => 'required|min:4',
+            'role'     => 'required'
+        ]);
+
+        // Later we will add database save code here (Step 5)
+        
+        return back()->with('success', 'Form received (Database yet to add)');
+    }
+
+}
