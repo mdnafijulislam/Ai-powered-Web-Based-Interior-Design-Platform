b6403436d8e83cb0133225c0bd2009ffc66dda7a
mdnafijulislam <islam22205101045@diu.edu.bd>
2025-11-27 16:35:13 +0600

    auth controller error remove

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -6,24 +6,6 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function showLogin()
-    {
-        return view('auth.login');
-    }
-
-    public function showRegister()
-    {
-        return view('auth.register');
-    }
-}
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class AuthController extends Controller
-{
     // Show Register Page
     public function showRegister() {
         return view('auth.register');
